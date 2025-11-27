12231d61345a620b43130ff2b9a2984721f42842
xieweichin <33629223+xieweichin@users.noreply.github.com>
2025-11-27 18:23:54 +0800

    Remove labels from toolbox category
    Removed labels for '初始化' and '基本馬達控制' from the toolbox category.

--- toolbox.js
+++ toolbox.js
@@ -3,19 +3,7 @@ module.exports = {
         {
             xml: `
 <category name="德中智慧小車" colour="#b784b3">
-
-    <label text="初始化"></label>
     <block type="badecar_init"></block>
-
-    <label text="基本馬達控制"></label>
-    <block type="badecar_motor_pwm">
-        <value name="SPEED">
-            <shadow type="math_number">
-                <field name="NUM">255</field>
-            </shadow>
-        </value>
-    </block>
-
 </category>
 `
         }
