2ba7cd299c61c86f46aade62022531ba3e213a1f
xieweichin <33629223+xieweichin@users.noreply.github.com>
2025-11-27 18:23:38 +0800

    Refactor generator.js for initialization changes
    Updated initialization function and removed motor PWM function.

--- generator.js
+++ generator.js
@@ -1,19 +1,7 @@
-/* BadeCar generators v02 */
-function registerGenerator(Blockly) {
+function registerGenerator (Blockly) {
 
-    // 初始化
     Blockly.Arduino['badecar_init'] = function () {
-        Blockly.Arduino.includes_['badecar_core'] = '#include <BadeCarCore.h>';
-        Blockly.Arduino.definitions_['badecar_core_obj'] = 'BadeCarCore core;';
-        return 'core.init();\n';
-    };
-
-    // 單一馬達 PWM
-    Blockly.Arduino['badecar_motor_pwm'] = function (block) {
-        var motor = block.getFieldValue('MOTOR_ID');
-        var speed = Blockly.Arduino.valueToCode(block, 'SPEED', Blockly.Arduino.ORDER_ATOMIC) || 0;
-
-        return 'core.setMotorPwm(' + motor + ', ' + speed + ');\n';
+        return '// 德中智慧小車初始化\n';
     };
 
     return Blockly;
